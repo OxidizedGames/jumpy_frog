5894844d1d384fb0c1c445c05184d89de6a7a345
James Bell <jamescarterbell@gmail.com>
2023-07-03 14:07:01 -0400

    Fix warnings

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -1,16 +1,37 @@
 use bevy::{
-    prelude::{Bundle, Component, Handle, Image, Transform},
+    prelude::{
+        info, Bundle, Camera, Commands, Component, Entity, GlobalTransform, Handle, Image,
+        IntoSystemConfigs, Plugin, Query, Transform, Without,
+    },
     sprite::SpriteBundle,
 };
-use bevy_rapier2d::prelude::{Collider, ExternalImpulse, RigidBody};
+use bevy_rapier2d::prelude::{Collider, LockedAxes, RigidBody, Velocity};
+use leafwing_input_manager::prelude::ActionState;
+
+use crate::controls::{FrogActions, FrogAimState};
+
+use self::physics::{CollisionState, PhysicsPlugin};
+
+mod physics;
+
+pub struct GameplayPlugin;
+
+impl Plugin for GameplayPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_plugin(PhysicsPlugin)
+            .add_systems((start_aim, update_aim, finish_aim).chain());
+    }
+}
 
 #[derive(Bundle)]
 pub struct FrogBundle {
     rigid_body: RigidBody,
     collider: Collider,
-    sprite: SpriteBundle,
     jump_strength: JumpStrength,
-    impulse: ExternalImpulse,
+    impulse: Velocity,
+    locked_axes: LockedAxes,
+    collision_state: CollisionState,
+    sprite: SpriteBundle,
 }
 
 impl FrogBundle {
@@ -18,16 +39,82 @@ impl FrogBundle {
         Self {
             rigid_body: RigidBody::Dynamic,
             collider: Collider::cuboid(8.0, 8.0),
+            jump_strength: JumpStrength(10.0),
+            impulse: Velocity::default(),
+            locked_axes: LockedAxes::ROTATION_LOCKED,
+            collision_state: CollisionState::default(),
             sprite: SpriteBundle {
                 texture,
                 transform,
                 ..Default::default()
             },
-            jump_strength: JumpStrength(10.0),
-            impulse: ExternalImpulse::default(),
         }
     }
 }
 
 #[derive(Component)]
 pub struct JumpStrength(pub f32);
+
+fn start_aim(
+    mut commands: Commands,
+    frogs: Query<(Entity, &ActionState<FrogActions>), Without<FrogAimState>>,
+) {
+    for (entity, action_state) in frogs.iter() {
+        if action_state.just_pressed(FrogActions::Aim) {
+            if let Some(cursor_pos) = action_state.axis_pair(FrogActions::AimPositionScreen) {
+                commands
+                    .entity(entity)
+                    .insert(FrogAimState::new(cursor_pos.xy()));
+                info!("Starting Frog Aim: {}", cursor_pos.xy());
+            }
+        }
+    }
+}
+
+fn update_aim(mut frogs: Query<(&ActionState<FrogActions>, &mut FrogAimState)>) {
+    for (action_state, mut aim_state) in frogs.iter_mut() {
+        if action_state.pressed(FrogActions::Aim) {
+            if let Some(cursor_pos) = action_state.axis_pair(FrogActions::AimPositionScreen) {
+                aim_state.end_pos = cursor_pos.xy();
+                info!("Updating Frog Aim: {}", aim_state.end_pos);
+            }
+        }
+    }
+}
+
+fn finish_aim(
+    mut commands: Commands,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    mut frogs: Query<(
+        Entity,
+        &ActionState<FrogActions>,
+        &FrogAimState,
+        &JumpStrength,
+        &CollisionState,
+        &mut Velocity,
+    )>,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    for (entity, action_state, aim_state, jump_strength, collision_state, mut velocity) in
+        frogs.iter_mut()
+    {
+        if action_state.just_pressed(FrogActions::AimRelease) {
+            if collision_state.bottom {
+                let start_pos = camera
+                    .viewport_to_world_2d(camera_transform, aim_state.start_pos)
+                    .unwrap();
+                let end_pos = camera
+                    .viewport_to_world_2d(camera_transform, aim_state.end_pos)
+                    .unwrap();
+                let direction = start_pos - end_pos;
+                velocity.linvel = direction.clamp_length_max(32.0) * jump_strength.0;
+                info!("Launching Frog with impulse: {}", velocity.linvel);
+            }
+            commands.entity(entity).remove::<FrogAimState>();
+        }
+        if action_state.just_pressed(FrogActions::AimCancel) {
+            commands.entity(entity).remove::<FrogAimState>();
+            info!("Cancelling Frog Launch");
+        }
+    }
+}
---
+++ src/gameplay/physics.rs
@@ -0,0 +1,100 @@
+use bevy::{
+    math::Vec2,
+    prelude::{
+        info, Component, Entity, GlobalTransform, IntoSystemConfig, IntoSystemSetConfig, Plugin,
+        Query, Res, SystemSet,
+    },
+};
+use bevy_rapier2d::prelude::{Collider, PhysicsSet, QueryFilter, RapierContext};
+
+pub struct PhysicsPlugin;
+
+#[derive(Hash, Debug, PartialEq, Eq, Clone, SystemSet)]
+struct PostPhysicsSet;
+
+impl Plugin for PhysicsPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_system(
+            update_collision_state
+                .no_default_base_set()
+                .in_set(PostPhysicsSet),
+        )
+        .configure_set(PostPhysicsSet.after(PhysicsSet::Writeback));
+    }
+}
+
+#[derive(Component, Default, Debug)]
+pub struct CollisionState {
+    pub top: bool,
+    pub bottom: bool,
+    pub left: bool,
+    pub right: bool,
+}
+
+fn update_collision_state(
+    context: Res<RapierContext>,
+    mut collision_states: Query<(Entity, &mut CollisionState, &GlobalTransform, &Collider)>,
+) {
+    for (entity, mut collision_state, transform, collider) in collision_states.iter_mut() {
+        *collision_state = CollisionChecker::new(&context, transform, collider)
+            .with_filter(
+                QueryFilter::default()
+                    .exclude_collider(entity)
+                    .exclude_rigid_body(entity),
+            )
+            .get_collision_state();
+    }
+}
+
+struct CollisionChecker<'a> {
+    context: &'a RapierContext,
+    transform: &'a GlobalTransform,
+    collider: Collider,
+    filter: Option<QueryFilter<'a>>,
+}
+
+impl<'a> CollisionChecker<'a> {
+    fn new(
+        context: &'a RapierContext,
+        transform: &'a GlobalTransform,
+        collider: &'a Collider,
+    ) -> Self {
+        let mut checker = Self {
+            context,
+            transform,
+            collider: collider.clone(),
+            filter: None,
+        };
+        checker.collider.set_scale(Vec2::new(0.99, 0.99), 16);
+        checker
+    }
+
+    fn with_filter(self, filter: QueryFilter<'a>) -> Self {
+        Self {
+            filter: Some(filter),
+            ..self
+        }
+    }
+
+    fn check_collision(&self, direction: Vec2) -> bool {
+        self.context
+            .cast_shape(
+                self.transform.translation().truncate(),
+                0.0,
+                direction,
+                &self.collider,
+                0.1,
+                self.filter.unwrap_or_default(),
+            )
+            .is_some()
+    }
+
+    fn get_collision_state(&self) -> CollisionState {
+        CollisionState {
+            top: self.check_collision(Vec2::new(0.0, 1.0)),
+            bottom: self.check_collision(Vec2::new(0.0, -1.0)),
+            left: self.check_collision(Vec2::new(-1.0, 0.0)),
+            right: self.check_collision(Vec2::new(1.0, 0.0)),
+        }
+    }
+}
